644e721afc7a6745875eacc8673015fa49dcc8c7
Ruiwen <ruiwen.li@mail.utoronto.ca>
2021-04-09 19:25:45 -0400

    Add finetune, reorganize code base

--- utils.py
+++ utils.py
@@ -1,10 +1,8 @@
 import torch
 import argparse
+import numpy as np
 
-#DEVICE = torch.device('cpu')
-DEVICE = torch.device('cuda')   
-
-
+DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
@@ -13,3 +11,16 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def accuracy(y_pred, y_true):
+    "y_true is (batch_size) and y_pred is (batch_size, K)"
+    _, y_max_pred = y_pred.max(1)
+    correct = ((y_true == y_max_pred).float()).mean()
+    acc = correct * 100
+    return acc
+
+def get_class_distribution(dataset, args):
+    class_dist = torch.zeros(args.n_steering_classes)
+    for data in dataset:
+        class_dist[data['cmd']] += 1
+    return class_dist / torch.sum(class_dist)
